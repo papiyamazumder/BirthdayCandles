07c3708ba59a0416b0b99ef56e726e7cf75f911f
Papiya Mazumder <papiyamazumder2@gmail.com>
2024-01-14 22:26:02 +0530

    Add files via upload

---
+++ Birthday_Candles/Birthday_Candles/Program.cs
@@ -0,0 +1,14 @@
+﻿using Birthday_Candles;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> candles = new List<int>
+    {
+        8, 3, 2, 1, 3, 6, 8, 2, 8
+    };
+
+        int result = Result.birthdayCakeCandles(candles);
+        Console.WriteLine(result);
+    }
+}
---
+++ Birthday_Candles/Birthday_Candles/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday_Candles
+{
+    class Result
+    {
+        /*
+         * Complete the 'birthdayCakeCandles' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY candles as parameter.
+         */
+
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            //write your logic and return result value
+
+            int maxvalue = candles[0];
+            int maxvaluecount = 0;
+
+            foreach (int count in candles)
+            {
+                if (count > maxvalue)
+                {
+                    maxvalue = count;  
+                    maxvaluecount = 1;
+                }
+                else if (count == maxvalue)
+                {
+                    maxvaluecount++;
+                }               
+            }           
+            return maxvaluecount;
+
+
+            // OR
+
+            // using .Max function -> It automatically sorts and filters and sets the maximum no. in the list/array.
+
+            int maxvalue1 = candles.Max();
+            int maxvaluecount1 = 0;
+
+            foreach (int count1 in candles)
+            {
+                if (candles[count1] == maxvalue1)
+                {
+                    maxvaluecount1++;
+                }
+            }
+            return maxvaluecount1;
+
+        }
+
+    }
+}
